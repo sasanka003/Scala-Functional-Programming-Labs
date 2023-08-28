765906c0541e44ebc9861ffa1e0ebc1bfd907978
sasanka udana <sasankaudana@gmail.com>
2023-08-29 01:32:13 +0530

    Added calculateAverage as a function

--- Lab10/Q1.scala
+++ Lab10/Q1.scala
@@ -1,14 +1,19 @@
 import scala.io.StdIn
 
 object AverageFahrenheitTemperature {
+
+  def calculateAverage(temperatureList: List[Double]) : Double = {
+    val toFahrenheit = (x: Double) => (x * 9 / 5) + 32
+
+    val sumAndCount = temperatureList.map(x => (toFahrenheit(x), 1)).reduce((a, b) => (a._1 + b._1, a._2 + b._2))
+    sumAndCount._1/sumAndCount._2
+  }
+
   def main(args: Array[String]): Unit = {
     print("Input: ")
     val input = scala.io.StdIn.readLine().stripPrefix("[").stripSuffix("]").split(",").map(_.trim.toDouble).toList
 
-    val toFahrenheit = (x: Double) => (x*9/5)+32
-
-    val sumAndCount = input.map(x => (toFahrenheit(x), 1)).reduce((a, b) => (a._1 + b._1, a._2 + b._2))
-    print("Output: Average Fahrenheit temperature: "+ (sumAndCount._1/sumAndCount._2) )
+    print("Output: Average Fahrenheit temperature: "+ calculateAverage(input) )
 //    print(sumAndCount)
   }
 }
